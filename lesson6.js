abc68375505a7c853edbd17aacece9c4e983a29f
Kate Kuksa <kuksa1406@gmail.com>
2023-11-23 16:21:02 +0500

    homework

--- lesson6.js
+++ lesson6.js
@@ -3,18 +3,36 @@
     Створіть функцію, яка буде повертати другий елемент з масиву
 */
 
+var arr = ["I","You","They","He","It"]
+console.log(arr[1])
+
 /* Task - 2
     Створіть функцію, який додасть 1 елемент до масиву з попередньої задачи (array1) (наприклад, "new item")
 */
+arr[5] = 'array1'
+console.log(arr)
 
 /* Task - 3
     Створіть другий масив (array2), потім створить функцію, яка отримує 2 масиви і повертає масив, що містить елементи з масивів array1 та array2
 */
+var array2 = ["She","We","Us","Them","People"];
+function mergeArrays(arr1, arr2) {
+var mergedArray = arr1.concat(arr2);
+return mergedArray;
+}
+console.log(mergeArrays(arr, array2)); 
 
 /* Task - 4 (Not required)
     Створіть функцію, яка буде повертає новий масив заданої довжини, заповнений заданим значенням
     https://www.w3schools.com/jsref/jsref_fill.asp
+
+
 */
+
+function createArray(length, value) {
+    return Array(length).fill(value);
+    }
+    console.log(createArray(5, "me"));
 
 /* Task - 5 (Not required)
     Створіть функцію , яка отримує масив об'єктів
@@ -22,12 +40,33 @@
     і повертає кількість людей, чиє ім'я "John" або вік менший за 30
      не використовуйте цикли
 */
+function countYoungAndJohnPeople(people) {
+    return people.filter(person => person.name.includes("John") || person.age < 30).length;
+    }
+    
+    var peopleArray = [
+    {name: "John Snow",
+    age: 45},
+    {name: "Cersei Lannister", 
+    age: 32},
+    {name: "Daenerys Targaryen",
+     age: 18},
+    {name: "Jorah Mormont", 
+    age: 28}
+    ];
+    
+    console.log(countYoungAndJohnPeople(peopleArray));
 
 /* Task - 6 (Not required)
     Створіть функцію , яка отримує масив об'єктів
     наприклад, [{name: "John Snow", age: 45}, {name: "Cersei Lannister", age: 32}, {name: "Daenerys Targaryen", age: 18}, {name: "Jorah Mormont", age: 28}]]
     і повертає новий масив усіх людей, старших за 30 років
 */
+function getPeopleOver30(people) {
+    return people.filter(person => person.age > 30);
+    }
+    
+    console.log(getPeopleOver30(peopleArray));
 
 
 /* Task - 7 (Not required)
@@ -42,6 +81,19 @@
  https://www.w3schools.com/jsref/jsref_map.asp
 */
 
+function getNameArray(people) {
+    return people.map(person => person.name);
+    }
+    
+    var peopleArray2 = [
+    {name: "Kate", 
+    age: 22},
+    {name: "Radik",
+     age: 32}
+    ];
+    
+    console.log(getNameArray(peopleArray2)); 
+
 /* Task - 8 (Not required)
 
     Створити функцію для знаходження індексу мінімального елементу масиву;
@@ -53,8 +105,10 @@
    Створити функцію, яка поверне кількість допустимих (НЕ not null/undefined) значень масиву
 */
 
+
 /* Task - 10 (Not required)
 
    Створити функцію, яка відфільтрує нульові та невизначені значення
   якщо кількість невірних записів більше 3 - потрібно згенерувати помилку "Дані потрібно переглянути"
 */
+
